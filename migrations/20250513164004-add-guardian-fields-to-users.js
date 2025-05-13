1d3c9e5ac41d3ebb225c2c7fa9017b7838db91a7
PRAVIN ABASAHEB KHAIRE <khairepravin14@gmail.com>
2025-05-13 22:18:05 +0530

    Update 20250513164004-add-guardian-fields-to-users.js

--- migrations/20250513164004-add-guardian-fields-to-users.js
+++ migrations/20250513164004-add-guardian-fields-to-users.js
@@ -1,20 +1,38 @@
-await queryInterface.addColumn('users', 'contactNumber', {
-  type: Sequelize.STRING(15),
-  allowNull: true
-});
-await queryInterface.addColumn('users', 'dateOfBirth', {
-  type: Sequelize.DATEONLY,
-  allowNull: true
-});
-await queryInterface.addColumn('users', 'guardianName', {
-  type: Sequelize.STRING(50),
-  allowNull: true
-});
-await queryInterface.addColumn('users', 'guardianContactNumber', {
-  type: Sequelize.STRING(15),
-  allowNull: true
-});
-await queryInterface.addColumn('users', 'guardianRelation', {
-  type: Sequelize.STRING(30),
-  allowNull: true
-});
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users', 'contactNumber', {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('users', 'dateOfBirth', {
+      type: Sequelize.DATEONLY,
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('users', 'guardianName', {
+      type: Sequelize.STRING(50),
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('users', 'guardianContactNumber', {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('users', 'guardianRelation', {
+      type: Sequelize.STRING(30),
+      allowNull: true
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('users', 'contactNumber');
+    await queryInterface.removeColumn('users', 'dateOfBirth');
+    await queryInterface.removeColumn('users', 'guardianName');
+    await queryInterface.removeColumn('users', 'guardianContactNumber');
+    await queryInterface.removeColumn('users', 'guardianRelation');
+  }
+};
