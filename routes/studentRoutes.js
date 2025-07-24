d5d4355ae407aab4240c29c3d9a2caa2db711a86
PRAVIN ABASAHEB KHAIRE <khairepravin14@gmail.com>
2025-07-24 12:40:44 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const studentController = require('../controllers/studentController');
+const authMiddleware = require('../middleware/authMiddleware'); // Import the auth middleware
 
 const router = express.Router();
 
 // Student login route
 router.post('/login', studentController.studentLogin);
-router.get('/tests', studentController.getTestsForStudent); // Get tests for student
-router.post('/solve-test', studentController.solveTest);
+router.get('/tests', authMiddleware,studentController.getTestsForStudent); // Get tests for student
+router.post('/solve-test',authMiddleware, studentController.solveTest);
 module.exports = router;
