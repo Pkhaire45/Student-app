d896ca10a8ec3a3a60a7e78381ce9f5b2bc5ae5f
PRAVIN ABASAHEB KHAIRE <khairepravin14@gmail.com>
2025-12-23 20:48:30 +0530

    Update batchRoutes.js

--- routes/batchRoutes.js
+++ routes/batchRoutes.js
@@ -4,9 +4,8 @@ const batchController = require('../controllers/batchController');
 
 router.post('/', batchController.createBatch);
 router.get('/', batchController.getAllBatches);
-// Update batch
-router.put('/:batchId', batchController.updateBatch);
 
+router.put('/:batchId', batchController.updateBatch);
 router.delete('/:batchId', batchController.deleteBatch);
 
 router.post('/:batchId/students', batchController.addStudentsToBatch);
